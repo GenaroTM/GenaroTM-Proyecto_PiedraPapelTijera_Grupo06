7f233a2fc4c7205183b483be6d7b16f9307a9f43
JoseCastro <30935971+Josepheum@users.noreply.github.com>
2022-09-24 22:43:30 -0300

    Prueba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite <code>src/App.js</code> y grabe si se le place.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprenda React si Ud. quiere
         </a>
       </header>
     </div>
