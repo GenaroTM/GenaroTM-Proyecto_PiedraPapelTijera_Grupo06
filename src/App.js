ace991268d27bb456fbad23b978dd5afe5b0b60a
JoseCastro <30935971+Josepheum@users.noreply.github.com>
2022-09-26 11:20:01 -0300

    Adición Imágenes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Opcion from './Opcion';
 import './App.css';
 import Resultado from './Resultado';
@@ -7,19 +7,30 @@ function App(){
 	
 	const [eleccionJugador, setEleccionJugador] = useState({});
 	const [eleccionMaquina, setEleccionMaquina] = useState({});
+	//const [imagen, setImagen]= useState([]);
+	const papel = require(`./Papel.png`);
+	const piedra = require(`./Piedra.png`);
+	const tijera = require(`./Tijera.png`);
+
+	
+	
 	
 	const opciones = [
 	{
 		eleccion: "piedra",
-		derrota: "tijera"
+		derrota: "tijera",
+		imagen:[(<img src={piedra} className="piedra" alt="piedra" />)]
 	},
 	{
 		eleccion: "papel",
-		derrota: "piedra"
+		derrota: "piedra",
+		imagen:[(<img src={papel} className="papel" alt="papel" />)]
+
 	},
 	{
 		eleccion: "tijera",
-		derrota: "papel"
+		derrota: "papel",
+		imagen:[(<img src={tijera} className="tijera" alt="tijera" />)]
 	}
 	]
 	
@@ -39,15 +50,18 @@ function App(){
 	return (
 	<body className="body">
 	<div className="App">
-	<Resultado jugador={eleccionJugador} maquina={eleccionMaquina}/>
+	<Resultado className="resultadoFinal" jugador={eleccionJugador} maquina={eleccionMaquina}/>
 	<main>
 	<section>
 		<div className="Jugador"> Jugador </div>
 		<div className="eleccion"> {eleccionJugador.eleccion} </div>
+		<div className="eleccion"> {eleccionJugador.imagen} </div>
 	</section>
 	<section>
 		<div className="maquina"> Maquina </div>
 		<div className="eleccion"> {eleccionMaquina.eleccion}</div>
+		<div className="eleccion"> {eleccionMaquina.imagen} </div>
+
 	</section>
 	</main>
 	
@@ -61,7 +75,14 @@ function App(){
 		
 	
 	</div>
+	<footer className="footer">
+	<h3 > Integrantes</h3>
+	<h4 className="integrantes"> Alfonso Ulises - Genaro Montiel - Lisandro Corredoyra - Jorgelin Chañi - Jose Castro - Emanuel Avila </h4>
+	</footer>
 	</body>
+	
+	
+	
 	);
 }
 
